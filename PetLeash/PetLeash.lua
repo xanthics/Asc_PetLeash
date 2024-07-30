596938152793060b68ab8f0dcb1ec814b9d3cec4
xanthics <3137623+xanthics@users.noreply.github.com>
2024-07-30 14:38:13 -0700

    change db check to after db is initialized if missing.

--- PetLeash/PetLeash.lua
+++ PetLeash/PetLeash.lua
@@ -478,8 +478,8 @@ local function migrateData()
 end
 
 function addon:OnInitialize()
-	migrateData()
 	self.db = LibStub("AceDB-3.0"):New("PetLeashDB", defaults)
+	migrateData()
 	self.db.RegisterCallback(self, "OnProfileChanged", "OnProfileChange")
 	self.db.RegisterCallback(self, "OnProfileCopied", "OnProfileChange")
 	self.db.RegisterCallback(self, "OnProfileReset", "OnProfileChange")
